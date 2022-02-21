22d82ce5e3fd03073e455d72082fa7b09e35a1a7
Kristóf <kristef.g@gmail.com>
2022-02-21 13:02:38 +0100

    firebase added

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDepSmWqOpiRrWZzDtU7nKHTp6ifn6l9YY",
+  authDomain: "portfolio-b47bf.firebaseapp.com",
+  projectId: "portfolio-b47bf",
+  storageBucket: "portfolio-b47bf.appspot.com",
+  messagingSenderId: "655251267106",
+  appId: "1:655251267106:web:82c600a94d7ac28946f47b",
+  measurementId: "G-E2HX8659LC",
+};
+
+export const firebaseAnalytics = firebase.analytics();
